9d5aab3b816d49813d6d2ffbb49e0a2957c120e2
Sarah Bawabe <sarah_bawabe@brown.edu>
2021-02-28 19:31:25 -0500

    remove duplicates take 2

--- kdtree.py
+++ kdtree.py
@@ -17,7 +17,7 @@ def build_nneigh_graph(coords, radius, simple=False):
 
     kdtree = spatial.KDTree(coords)
     neighs = kdtree.query_ball_point(coords, radius)
-    
+
     graph = None
     if simple:
         graph = build_simple_graph(coords)
@@ -28,18 +28,7 @@ def build_nneigh_graph(coords, radius, simple=False):
     for i in range(len(neighs)):
         if len(neighs[i]) > 1: # if has neighbors within radius
             for j in range(len(neighs[i])):
-                if i != j and not graph.has_edge(i,j): # ensure no self-loops or duplicates
+                if i != j and not graph.has_edge(j,i): # ensure no self-loops or duplicates
                     graph.add_edge(i, j)
 
     return graph
-
-
-if __name__ == '__main__':
-    obj_list = bOb.convert_to_obj_list(m, coords_matrix, vels_matrix)
-    # obj_list = bOb.generate_rand_obj_list(N=10,ndim=3)
-    # obj_list = [bOb.bodyObject(1,np.array([1,1,1]),np.array([1,1,1])), bOb.bodyObject(1,np.array([2,2,2]),np.array([1,1,1]))]
-    nBody = NBody(obj_list, ndim=3, iters=10)
-    nBody.perform_simulation()
-    nneigh_graph = build_nneigh_graph(nBody.pos_matrix[:,:,0], 25)
-    plot_graph(nneigh_graph)
-    plot_3d_graph(nneigh_graph)
